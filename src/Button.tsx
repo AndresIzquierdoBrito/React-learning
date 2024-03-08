ec898948b5ea8672060e9d54b9f987b9670c8eac
Andrew <andres.izbri@gmail.com>
2024-03-08 17:22:48 +0100

    React

---
+++ src/Button.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'flowbite-react';
+const styles = {
+  marginLeft: '50rem',
+};
+
+export default function MyPage() {
+  return (
+    <div>
+      <Button style={styles}>Click me</Button>
+      
+    </div>
+  );
+}
